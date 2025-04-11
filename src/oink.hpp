b4934078e6f68228bf6c98ff06e31b435b1091a6
Yurii Rashkovskii <yrashk@gmail.com>
2025-04-11 09:29:25 -0700

    Problem: interprocess communication
    It's often a non-trivial problem to share data structures and send messages
    between processes.
    
    Solution: build a thin library to do just that
    
    There are some very exciting projects like
    [Flow-IPC](https://github.com/Flow-IPC/ipc) but they are currently not
    suitable for our needs because of lack of arm64/macos support. Arguably,
    this is fixable, but then its perceived build surface complexity comes
    into the picture (it's a big library). It does have amazing benefits,
    though, like transmitting C++ structures over the wire.
    
    Oink is an attempt to make a simple library that works well enough, fast
    enough. Most of workloads sent through this will be terminated by a
    database operation (as the use case for this library is Omnigres), and
    the hypothesis here is that the overhead of this IPC will be negligible
    comparatively.
    
    If this hypothesis won't be correct, we can try and make use of
    Flow-IPC, after all. For now, the job is to make something workable that
    Omnigres components can share.

---
+++ src/oink.hpp
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <map>
+#include <typeindex>
+
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/ipc/message_queue.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+#include <boost/interprocess/sync/interprocess_upgradable_mutex.hpp>
+
+#include <boost/container/vector.hpp>
+
+namespace oink {
+namespace bip = boost::interprocess;
+namespace bc = boost::container;
+
+template <typename T>
+using allocator = bip::allocator<T, bip::managed_shared_memory::segment_manager>;
+
+template <typename Container, typename Mutex> struct shared_container {
+  using container_type = Container;
+  using mutex_type = Mutex;
+  Container container;
+  Mutex mutex;
+
+  template <typename... Args>
+  explicit shared_container(Args &&...args) : container(std::forward<Args>(args)...) {}
+
+  operator container_type &() { return container; }
+  operator mutex_type &() { return mutex; }
+};
+
+template <typename T>
+concept message = requires(T t) {
+  { T::name() } -> std::same_as<const char *>;
+};
+
+struct endpoint {
+  struct msg {
+    std::size_t hash;
+    bip::offset_ptr<void> message_;
+  };
+
+  using msg_allocator_t = bip::allocator<msg, bip::managed_shared_memory::segment_manager>;
+  using msg_vec =
+      shared_container<bc::vector<msg, msg_allocator_t>, bip::interprocess_upgradable_mutex>;
+
+  struct header {};
+
+  using header_t = shared_container<header, bip::interprocess_upgradable_mutex>;
+
+  endpoint(const char *segment_name, size_t segment_size, const char *mq_segment_name,
+           size_t mq_max_messages)
+      : segment(bip::open_or_create, segment_name, segment_size),
+        mq(bip::open_or_create, mq_segment_name, mq_max_messages, sizeof(std::size_t)),
+        header_(segment.find_or_construct<header_t>("__header")()),
+        msgs_(segment.find_or_construct<msg_vec>("__msgs")(segment.get_segment_manager())) {}
+
+  auto get_segment_manager() { return segment.get_segment_manager(); }
+
+  template <typename T> auto get_allocator() {
+    return bip::allocator<T, bip::managed_shared_memory::segment_manager>(
+        segment.get_segment_manager());
+  }
+
+protected:
+  bip::managed_shared_memory segment;
+  bip::message_queue mq;
+
+  header_t *header_;
+  msg_vec *msgs_;
+};
+
+struct sender : endpoint {
+  using endpoint::endpoint;
+
+  template <message M, typename... Args> M &send(Args &&...args) {
+    auto msg_ = get_allocator<M>().allocate(1);
+    std::construct_at(msg_.get(), std::forward<Args>(args)...);
+    bip::scoped_lock lock(msgs_->mutex);
+    auto m = msgs_->container.emplace(msgs_->container.end(),
+                                      msg{std::hash<std::string>{}(M::name()), msg_});
+    size_t i = msgs_->container.index_of(m);
+    mq.send(&i, sizeof(i), 0);
+    return *msg_;
+  }
+};
+
+struct receiver : endpoint {
+  using endpoint::endpoint;
+
+  template <message... Msg> bool receive(auto visitor) {
+    bip::message_queue::size_type recvd_size;
+    unsigned int priority;
+
+    std::size_t i;
+    if (mq.timed_receive(&i, sizeof(i), recvd_size, priority,
+                         std::chrono::system_clock::now() + std::chrono::milliseconds(500))) {
+
+      bool matched = false;
+
+      bip::scoped_lock lock(msgs_->mutex);
+      auto &j = msgs_->container.at(i);
+
+      (try_handle<Msg>(j, matched, visitor), ...);
+
+      if (!matched) {
+        // handle unknown message type if necessary
+      } else {
+      }
+
+      if (mq.get_num_msg() == 0) {
+        msgs_->container.clear();
+      }
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+private:
+  template <message T> void try_handle(msg &j, bool &matched, auto visitor) {
+    if (j.hash == std::hash<std::string>{}(T::name())) {
+      visitor(*(static_cast<T *>(j.message_.get())));
+      get_allocator<T>().deallocate(bip::offset_ptr<T>(static_cast<T *>(j.message_.get())), 1);
+      matched = true;
+    }
+  }
+};
+
+} // namespace oink
---
+++ tests/oink.cpp
@@ -0,0 +1,98 @@
+#include "doctest.h"
+
+#include <iostream>
+#include <thread>
+
+#include <oink.hpp>
+
+#include <boost/container/string.hpp>
+
+template <class... Ts> struct overloaded : Ts... {
+  using Ts::operator()...;
+};
+template <class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+TEST_CASE("smoke test") {
+  oink::bip::shared_memory_object::remove("oink_test");
+  oink::bip::shared_memory_object::remove("oink_test_mq");
+  oink::bip::remove_shared_memory_on_destroy _test("oink_test");
+  oink::bip::remove_shared_memory_on_destroy _test_mq("oink_test_mq");
+
+  struct mymsg {
+    static constexpr const char *name() { return "msg"; }
+    int i;
+  };
+
+  struct mymsg1 {
+    static constexpr const char *name() { return "msg1"; }
+    oink::bc::basic_string<char, std::char_traits<char>, oink::allocator<char>> message;
+
+    mymsg1(const char *msg, const oink::allocator<char> &alloc) : message(msg, alloc) {}
+  };
+
+  oink::sender endpoint("oink_test", 65536, "oink_test_mq", 1024);
+
+  auto m = endpoint.send<mymsg>(123);
+  CHECK(m.i == 123);
+  mymsg1 m1 = endpoint.send<mymsg1>("allocator", endpoint.get_allocator<char>());
+
+  CHECK(m1.message == "allocator");
+
+  oink::receiver rendpoint("oink_test", 65536, "oink_test_mq", 1024);
+  int received = 0;
+  CHECK(rendpoint.receive<mymsg, mymsg1>(
+      overloaded{[&](mymsg &msg) { received = msg.i; }, [](mymsg1 &msg) {}}));
+
+  CHECK(received == m.i);
+
+  std::string s;
+  CHECK(rendpoint.receive<mymsg, mymsg1>(
+      overloaded{[&](mymsg &msg) { received = msg.i; }, [&](mymsg1 &msg) { s = msg.message; }}));
+  CHECK(s == "allocator");
+}
+
+TEST_CASE("smoke test (multithreading)") {
+  oink::bip::shared_memory_object::remove("oink_test");
+  oink::bip::shared_memory_object::remove("oink_test_mq");
+  oink::bip::remove_shared_memory_on_destroy _test("oink_test");
+  oink::bip::remove_shared_memory_on_destroy _test_mq("oink_test_mq");
+
+  struct mymsg {
+    static constexpr const char *name() { return "msg"; }
+    int i;
+  };
+
+  struct stop {
+    static constexpr const char *name() { return "stop"; }
+  };
+
+  oink::sender endpoint("oink_test", 65536 * 100, "oink_test_mq", 1024);
+
+  std::vector<int> values;
+  std::thread rt([&]() {
+    oink::receiver rendpoint("oink_test", 65536 * 100, "oink_test_mq", 1024);
+    bool done = false;
+    while (!done) {
+      rendpoint.receive<mymsg, stop>(
+          overloaded{[&](mymsg &msg) { values.push_back(msg.i); }, [&](stop &) { done = true; }});
+    }
+  });
+
+  std::vector<std::thread> threads;
+  for (int i = 0; i < 100; i++) {
+    threads.emplace_back([&](int x) { endpoint.send<mymsg>(x); }, i);
+  }
+
+  for (auto &t : threads) {
+    t.join();
+  }
+  endpoint.send<stop>();
+
+  rt.join();
+  CHECK(values.size() == 100);
+  std::sort(values.begin(), values.end());
+  for (int i = 0; auto &val : values) {
+    CHECK(values[i] == i);
+    i++;
+  }
+}
