730d2491c71df33947da008a2489c753baa18dd2
Yurii Rashkovskii <yrashk@gmail.com>
2025-04-20 09:18:34 -0700

    Problem: inclusion of boost interprocess shared_ptr header
    We don't really use it right now.
    
    Solution: remove it

--- src/oink.hpp
+++ src/oink.hpp
@@ -6,7 +6,6 @@
 #include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
-#include <boost/interprocess/smart_ptr/shared_ptr.hpp>
 #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 #include <boost/interprocess/sync/interprocess_upgradable_mutex.hpp>
 
